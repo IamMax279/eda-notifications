3931930e13b6c2ea26ae243ec72e6d77be26e86d
IamMax279 <maksssiula@gmail.com>
2025-06-13 20:23:43 +0200

    add security filter chain config

--- user-service/src/main/java/com/max420/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/max420/user_service/UserServiceApplication.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EntityScan(basePackages = {"com.max420.user_service.models"})
+@ComponentScan(basePackages = {
+		"com.max420.user_service.controller",
+		"com.max420.user_service.repository",
+		"com.max420.user_service.services",
+		"com.max420.user_service.config"
+})
 @SpringBootApplication
 public class UserServiceApplication {
 
--- user-service/src/main/java/com/max420/user_service/config/SecurityConfig.java
+++ user-service/src/main/java/com/max420/user_service/config/SecurityConfig.java
@@ -2,11 +2,29 @@ package com.max420.user_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("/user/create-user")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .formLogin(customizer -> customizer.disable())
+                .httpBasic(customizer -> customizer.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
